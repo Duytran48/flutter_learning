e5412dd0e4045acc129e8f624af1834cdac4b1e4
DuyPCadmin <duyprogaming47@gmail.com>
2026-05-04 16:52:47 +0700

    wrong folder

--- tuan2/tuan_2/README.md
+++
@@ -1,3 +0,0 @@
-# tuan_2
-
-A new Flutter project.
--- tuan2/tuan_2/lib/data/model.dart
+++
@@ -1,9 +0,0 @@
-class ProductModel {
-  int? id;
-  String? name;
-  int? price;
-  String? img;
-  String? des;
-
-  ProductModel({this.id, this.name, this.price, this.img, this.des});
-}
